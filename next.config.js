793d47ff47d0622d779fc6b8d2d1170c3baf33c3
Clyde Tadiwa <111828237+taa217@users.noreply.github.com>
2023-09-26 10:52:48 +0200

    Update next.config.js
    Set topLevelAwait to true

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    topLevelAwait: true,
-  },
-}
 
-module.exports = nextConfig
+module.exports = {
+  webpack: (config) => {
+    // this will override the experiments
+    config.experiments = { ...config.experiments, topLevelAwait: true };
+    // this will just update topLevelAwait property of config.experiments
+    // config.experiments.topLevelAwait = true 
+    return config;
+  },
+};
