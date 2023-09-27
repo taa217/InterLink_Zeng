be9e8c27ee39bb8da0d77cab6cada27aa73ce9e2
Clyde Tadiwa <111828237+taa217@users.noreply.github.com>
2023-09-27 09:32:26 +0200

    Update next.config.js
    Update tomeout for loop execution

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ module.exports = {
   webpack: (config) => {
     // this will override the experiments
     config.experiments = { ...config.experiments, topLevelAwait: true };
+    config.staticPageGenerationTimeout : 120;
     // this will just update topLevelAwait property of config.experiments
     // config.experiments.topLevelAwait = true 
     return config;
