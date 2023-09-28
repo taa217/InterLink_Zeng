7d1ffae320681d2ff24ee69ae7880b2a45f7da8f
Clyde Tadiwa <111828237+taa217@users.noreply.github.com>
2023-09-28 10:56:50 +0200

    Update next.config.js
    Upade timeout webpack

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   webpack: (config) => {
     // this will override the experiments
-    config.experiments = { ...config.experiments, topLevelAwait: true };
+    config.experiments = { ...config.experiments, topLevelAwait: true,
+                        staticPageGenerationTimeout: 120, };
     // this will just update topLevelAwait property of config.experiments
     // config.experiments.topLevelAwait = true 
     return config;
