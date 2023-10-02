cb63193731db4083236aeee68e3bd3d8e7ebb3d1
Clyde Tadiwa <111828237+taa217@users.noreply.github.com>
2023-10-02 11:59:22 +0200

    Update page.js
    Put styles to p tag

--- src/app/messages/page.js
+++ src/app/messages/page.js
@@ -1,6 +1,7 @@
 import MessageComponent from "../../../components/message"
 import Navbar from "../../../components/navbar"
 import RoutesTopbar from "../../../components/routesTopbar"
+import styles from '../../../components/AnsQuestion.module.css'
 
 
 function Messages(){
@@ -9,7 +10,7 @@ function Messages(){
             <RoutesTopbar route_name="Messages"/>
             <Navbar/>
             
-        <h1>YOU HAVE NO MESSAGES YET...</h1>
+        <h1 className={styles.cl}>YOU HAVE NO MESSAGES YET...</h1>
         </div>
     )
 }
