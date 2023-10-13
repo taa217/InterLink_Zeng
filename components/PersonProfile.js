c14981b4b73eec143bce8b4c6492995288e1e7d5
Clyde Tadiwa <111828237+taa217@users.noreply.github.com>
2023-10-12 20:53:43 -0700

    Update PersonProfile.js
    pic

--- components/PersonProfile.js
+++ components/PersonProfile.js
@@ -12,7 +12,7 @@ function PersonProfile(){
         <div className={styles.container}>
             <div className={styles.inner}>
             <div className={styles.cover}><Image src="/biztech.jpg" layout="fill" objectFit="cover" alt="cover"/> </div>
-          <div className={styles.it}><div className={styles.profile}><div></div><Image src="/postpics/pp.jpg" width={150} height={150} objectFit="cover" alt="profile" className={styles.profileimg} /></div>
+          <div className={styles.it}><div className={styles.profile}><div></div><Image src="/group.png" width={150} height={150} objectFit="cover" alt="profile" className={styles.profileimg} /></div>
           <div className={styles.msgnconnect}>
             <div className={styles.innerdiv}><div><button className={styles.btns}>Message</button></div><div><button className={styles.btns}>Connect</button></div></div>
             </div>
